8ed3ab65914d6c49ed761e150bb946a7936fe249
eduantunes266 <eduantunes266@gmail.com>
2026-02-12 20:02:52 +0000

    Dia 6 --> Ajustes de interface com Comando IV 100% funcional , inicio de codigo bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <WebServer.h>
 #include <LittleFS.h>
 #include <IRremote.hpp> 
+#include <BleKeyboard.h>
+
 
 const char* ssid = "MEO-99C9A0";       
 const char* password = "33d79a7da1";   
 
 const int PIN_EMISSOR = 26; 
-
+BleKeyboard bleKeyboard("Xiaomi ESP32", "Xiaomi", 100);
 WebServer server(80);
 
 uint32_t obterCodigoMEO(String comando) {
@@ -23,7 +25,7 @@ uint32_t obterCodigoMEO(String comando) {
   if (comando == "DOWN")       return 0xFE01F708; 
   if (comando == "LEFT")       return 0xFC03F708; 
   if (comando == "RIGHT")      return 0xFD02F708; 
-  if (comando == "OK")         return 0xAF50F708; 
+  if (comando == "OK")         return 0xE01FF708; 
   if (comando == "BACK")       return 0xE31CF708;
   if (comando == "HOME")       return 0xF30CF708;
   if (comando == "APPS_GRID")  return 0xF708F708; 
@@ -45,33 +47,69 @@ uint32_t obterCodigoMEO(String comando) {
   if (comando == "7") return 0xE817F708;
   if (comando == "8") return 0xE718F708;
   if (comando == "9") return 0xE619F708;
-
+  if (comando == "SWAP") return 0xAF50F708 ; 
   return 0; 
+}
+
+void enviarComandoBluetooth(String comando) {
+  if(!bleKeyboard.isConnected()) return;
+
+if (comando == "TV_UP")         bleKeyboard.write(KEY_UP_ARROW);
+  else if (comando == "TV_DOWN")  bleKeyboard.write(KEY_DOWN_ARROW);
+  else if (comando == "TV_LEFT")  bleKeyboard.write(KEY_LEFT_ARROW);
+  else if (comando == "TV_RIGHT") bleKeyboard.write(KEY_RIGHT_ARROW);
+  else if (comando == "TV_OK")    bleKeyboard.write(KEY_RETURN);
+  else if (comando == "TV_BACK")  bleKeyboard.write(KEY_ESC);
+  else if (comando == "TV_HOME")  bleKeyboard.write(KEY_MEDIA_WWW_HOME); 
+  
+  else if (comando == "TV_VOL_UP")   bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+  else if (comando == "TV_VOL_DOWN") bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+  else if (comando == "TV_MUTE")     bleKeyboard.write(KEY_MEDIA_MUTE);
+
+  else if (comando == "TV_SOURCE")   bleKeyboard.write(KEY_F2); 
+  else if (comando == "TV_SETTINGS") bleKeyboard.write(KEY_F4); 
+  else if (comando == "TV_YOUTUBE")  bleKeyboard.write(KEY_MEDIA_WWW_SEARCH);
+
+  else if (comando == "TV_0") bleKeyboard.write('0');
+  else if (comando == "TV_1") bleKeyboard.write('1');
+  else if (comando == "TV_2") bleKeyboard.write('2');
+  else if (comando == "TV_3") bleKeyboard.write('3');
+  else if (comando == "TV_4") bleKeyboard.write('4');
+  else if (comando == "TV_5") bleKeyboard.write('5');
+  else if (comando == "TV_6") bleKeyboard.write('6');
+  else if (comando == "TV_7") bleKeyboard.write('7');
+  else if (comando == "TV_8") bleKeyboard.write('8');
+  else if (comando == "TV_9") bleKeyboard.write('9');
 }
 
 void handleIR() {
   if (server.hasArg("cmd")) {
     String comandoNome = server.arg("cmd");
-    Serial.print("Comando recebido via Wi-Fi: ");
+    Serial.print("Comando recebido: ");
     Serial.println(comandoNome);
-
-    uint32_t codigoHex = obterCodigoMEO(comandoNome);
-
-    if (codigoHex != 0) {
-      Serial.printf("A enviar código HEX: 0x%08X\n", codigoHex);
-      
-   
-      IrSender.sendNECRaw(codigoHex, 0); 
-      
-      server.send(200, "text/plain", "OK");
-    } else {
-      Serial.println(" -> Comando não existe na lista.");
-      server.send(400, "text/plain", "Erro: Comando desconhecido");
+    if (comandoNome.startsWith("TV_")) {
+        Serial.println(" -> Encaminhado para Bluetooth");
+        enviarComandoBluetooth(comandoNome);
     }
+    else {
+        uint32_t codigoHex = obterCodigoMEO(comandoNome);
+        if (codigoHex != 0) {
+            Serial.printf(" -> Encaminhado para IR (0x%08X)\n", codigoHex);
+            IrSender.sendNECRaw(codigoHex, 0);
+        } else {
+            Serial.println(" -> Comando IR desconhecido/vazio");
+        }
+    }
+      
+    server.send(200, "text/plain", "OK");
   } else {
     server.send(400, "text/plain", "Falta cmd");
   }
 }
+
+
+
+
 
 // Servir os ficheiros do site (LittleFS)
 void handleFileRead() {
@@ -95,7 +133,8 @@ void setup() {
   Serial.begin(115200);
   
   IrSender.begin(PIN_EMISSOR);
-
+bleKeyboard.begin(); 
+  Serial.println("Bluetooth Iniciado! Podes emparelhar.");
   if(!LittleFS.begin()){
     Serial.println("Erro ao montar LittleFS! (Lembra-te de fazer Upload Filesystem Image)");
     return;
